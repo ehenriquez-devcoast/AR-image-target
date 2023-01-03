5af5e691edc3a83f6a2702d06b75232c5329f4b7
Enrique <ehenriquez@devcoast.co>
2023-01-03 13:29:46 -0500

    feat(calcio): added workflow of calcio.

--- scripts/tap-frasco.js
+++ scripts/tap-frasco.js
@@ -8,7 +8,11 @@ AFRAME.registerComponent('tap-frasco', {
         
         this.el.addEventListener('click', (event) => {
             let sceneEl = document.querySelector('a-scene');
+            let CalcioFrasco = document.getElementById('calcio-frasco-0')
+            let MultivitFrasco = document.getElementById('multivit-frasco-0')
+            let ComplejobFrasco = document.getElementById('complejob-frasco-0')
             let Frasco = document.getElementById(this.data.frasco)
+            let Close = document.getElementById('close-btn')
             // OCULTAR OTROS
             // Frasco.setAttribute('animation', {
             //     property: 'scale',
@@ -20,40 +24,145 @@ AFRAME.registerComponent('tap-frasco', {
             // console.log(this.data.frasco)
             console.log(Frasco)
             // MOSTRAR DATOS
-            setTimeout(() => {
                 if (this.data.frasco === ('multivit-frasco-' + this.data.target)) {
                 // MOSTRAR COMBINACION
                     // Frasco.remove();
-                    Frasco.setAttribute('animation', {
-                        property: 'scale',
-                        to: '0 0 0',
-                        easing: 'easeOutQuad',
-                        dur: 500,
-                    })
+                    // Frasco.setAttribute('animation', {
+                    //     property: 'scale',
+                    //     to: '0 0 0',
+                    //     easing: 'easeOutQuad',
+                    //     dur: 500,
+                    // })
+                    CalcioFrasco.remove();
+                    ComplejobFrasco.remove();
                 }
                 else if(this.data.frasco === ('calcio-frasco-' + this.data.target)){
                     console.log('clicked calcio')
-                    Frasco.setAttribute('animation', {
+
+                    Frasco.setAttribute("class","notClickable")
+
+                    MultivitFrasco.setAttribute('animation', {
                         property: 'scale',
                         to: '0 0 0',
                         easing: 'easeOutQuad',
                         dur: 500,
                     });
+                    ComplejobFrasco.setAttribute('animation', {
+                        property: 'scale',
+                        to: '0 0 0',
+                        easing: 'easeOutQuad',
+                        dur: 500,
+                    });
+                    
+                    setTimeout(() => {
+                        MultivitFrasco.remove();
+                        ComplejobFrasco.remove();
 
-                    let newElement = document.createElement('a-entity');
-    
-                    newElement.setAttribute('position', '-0.5 1 0')
-                    newElement.setAttribute('visible', 'false')
-                    newElement.setAttribute('scale', '2 2 2')
-                    newElement.setAttribute('gltf-model', '#multivit-combinacion-glb')
+                        Close.setAttribute('animation', {
+                            property: 'scale',
+                            to: '.5 .5 .5',
+                            easing: 'easeInQuad',
+                            dur: 500,
+                        });
 
-                    sceneEl.appendChild(newElement);
+                         // Marco
+                        let marco = document.createElement('a-image');
+        
+                        marco.setAttribute("position", "0 2.5 0")
+                        marco.setAttribute("height", "1")
+                        marco.setAttribute("width", "3")
+                        marco.setAttribute("src", "#marco-calcio")
+
+                        sceneEl.appendChild(marco);
+
+                        // crear Combinacion
+                        let Combinacion = document.createElement('a-image');
+                        
+                        Combinacion.setAttribute("position", "2 0.7 0")
+                        Combinacion.setAttribute("width", "3")
+                        Combinacion.setAttribute("height", "1")
+                        Combinacion.setAttribute("scale", "0 0 0")
+                        Combinacion.setAttribute("id", "#calcio-Combinacion")
+                        Combinacion.setAttribute("src", "#calcio-combinacion")
+
+                        sceneEl.appendChild(Combinacion);
+
+                        // crear Capsulas
+                        let Capsulas = document.createElement('a-image');
+                        
+                        Capsulas.setAttribute("position", "2 0.7 0")
+                        Capsulas.setAttribute("width", "3")
+                        Capsulas.setAttribute("height", "1")
+                        Capsulas.setAttribute("scale", "0 0 0")
+                        Capsulas.setAttribute("id", "#calcio-Capsulas")
+                        Capsulas.setAttribute("src", "#calcio-capsulas")
+
+                        sceneEl.appendChild(Capsulas);
+
+                        // crear Rutina
+                        let Rutina = document.createElement('a-image');
+                        
+                        Rutina.setAttribute("position", "2 0.7 0")
+                        Rutina.setAttribute("width", "3")
+                        Rutina.setAttribute("height", "1")
+                        Rutina.setAttribute("scale", "0 0 0")
+                        Rutina.setAttribute("id", "#calcio-Rutina")
+                        Rutina.setAttribute("src", "#calcio-rutina")
+
+                        sceneEl.appendChild(Rutina);
+
+
+
+                        // Combinacion
+                        let combinacion = document.createElement('a-entity');
+        
+                        combinacion.setAttribute("position", ".1 -1.7 0")
+                        combinacion.setAttribute("scale", "4 4 4")
+                        combinacion.setAttribute("class", "clickable")
+                        combinacion.setAttribute("gltf-model", "#calcio-combinacion-glb")
+                        combinacion.setAttribute("tap-model", "boton: 1; video: #calcio-Video; combinacion: #calcio-Combinacion; capsulas: #calcio-Capsulas; rutina: #calcio-Rutina")
+
+                        sceneEl.appendChild(combinacion);
+
+                        // Capsulas
+                        let capsulas = document.createElement('a-entity');
+        
+                        capsulas.setAttribute("position", "-.4 -2.65 0")
+                        capsulas.setAttribute("scale", "4 4 4")
+                        capsulas.setAttribute("class", "clickable")
+                        capsulas.setAttribute("gltf-model", "#calcio-capsulas-glb")
+                        capsulas.setAttribute("tap-model", "boton: 2; video: #calcio-Video; combinacion: #calcio-Combinacion; capsulas: #calcio-Capsulas; rutina: #calcio-Rutina")
+
+                        sceneEl.appendChild(capsulas);
+
+                        // Rutina
+                        let rutina = document.createElement('a-entity');
+        
+                        rutina.setAttribute("position", "-.07 -2.6 0")
+                        rutina.setAttribute("scale", "4 4 4")
+                        rutina.setAttribute("class", "clickable")
+                        rutina.setAttribute("gltf-model", "#calcio-rutina-glb")
+                        rutina.setAttribute("tap-model", "boton: 3; video: #calcio-Video; combinacion: #calcio-Combinacion; capsulas: #calcio-Capsulas; rutina: #calcio-Rutina")
+
+                        sceneEl.appendChild(rutina);
+
+                        // Video
+                        let video = document.createElement('a-entity');
+        
+                        video.setAttribute("position", "-.03 -2.7 0")
+                        video.setAttribute("scale", "4 4 4")
+                        video.setAttribute("class", "clickable")
+                        video.setAttribute("gltf-model", "#calcio-video-glb")
+                        video.setAttribute("tap-model", "boton: 4; video: G4rPlmV_hjc; combinacion: #calcio-Combinacion; capsulas: #calcio-Capsulas; rutina: #calcio-Rutina")
+
+                        sceneEl.appendChild(video);
+
+                    }, 500)
+
+                   
                 }
-            }, 500)
     
         })
     },
         
 });
-          
-// export {tapModelComponent}
--- scripts/tap-model.js
+++ scripts/tap-model.js
@@ -1,4 +1,3 @@
-// const tapModelComponent = () => ({
 AFRAME.registerComponent('tap-model', {
     schema: {
         boton: {default: '1'},
@@ -8,32 +7,17 @@ AFRAME.registerComponent('tap-model', {
         rutina: {default: '#multi-Rutina'},
     },
     init() {
-        // var sceneEl = document.querySelector('a-scene')
-        // console.log(sceneEl.querySelector('#corazon'))
-        // let target = sceneEl.querySelector('#multivit')
-        // const model = document.getElementById('corazon-glb')  // document.getElementById('corazon')
-    
-        /*window.screen.orientation.addEventListener("change", function(e) {
-            // Do something on change
-        console.log('The orientation of the screen is: ' + window.screen.orientation);
-        });*/
-        // const Sombra = document.getElementById('sombra-multivit')
-    
+        // CLICK EVENT
+
         this.el.addEventListener('click', (event) => {
-        /// / Create new entity for the new object
-        // const model = this.el  // .getObjectByName('#corazon-glb')
-        // console.log('1')
     
         const Combinacion = document.getElementById(this.data.combinacion)
         const Capsulas = document.getElementById(this.data.capsulas)
         const Rutina = document.getElementById(this.data.rutina)
         
-        // const btn3d = document.getElementById('text-' + this.data.boton)
-        
         // const Video = document.getElementById(this.data.video)
     
         // OCULTAR OTROS
-        // Combinacion.setAttribute('visible', 'false')
         Combinacion.setAttribute('animation', {
             property: 'scale',
             to: '0 0 0',
@@ -52,79 +36,58 @@ AFRAME.registerComponent('tap-model', {
             easing: 'easeOutQuad',
             dur: 500,
         })
-        // Video.setAttribute('animation', {
-        // property: 'scale',
-        // to: '0 0 0',
-        // easing: 'easeOutQuad',
-        // dur: 500,
-        // })
-    
+        
         // MOSTRAR DATOS
         setTimeout(() => {
             if (this.data.boton === '1') {
-            // MOSTRAR COMBINACION
-            // Combinacion.setAttribute('visible', 'false')
-            // Combinacion.setAttribute('visible', 'true')
-            Combinacion.setAttribute('animation', {
-                property: 'scale',
-                to: '.4 .4 .4',
-                easing: 'easeOutQuad',
-                dur: 800,
-            })
-    
-            // console.log(btn3d.getAttribute('position').x + ' ' + btn3d.getAttribute('position').y)
-            // Sombra.setAttribute('position', btn3d.getAttribute('position').x + ' ' + (btn3d.getAttribute('position').y+0.1) + ' -0.2' )
-            // Sombra.setAttribute('scale', '.3 .3 .3');
-            
+                // MOSTRAR COMBINACION
+                Combinacion.setAttribute('animation', {
+                    property: 'scale',
+                    to: '1 1 1',
+                    easing: 'easeOutQuad',
+                    dur: 800,
+                })
             
             } else if (this.data.boton === '2') {
-            // MOSTRAR CAPSULAS
-            Capsulas.setAttribute('animation', {
-                property: 'scale',
-                to: '.4 .4 .4',
-                easing: 'easeOutQuad',
-                dur: 800,
-            })
+                // MOSTRAR CAPSULAS
+                Capsulas.setAttribute('animation', {
+                    property: 'scale',
+                    to: '1 1 1',
+                    easing: 'easeOutQuad',
+                    dur: 800,
+                })
             } else if (this.data.boton === '3') {
-            // MOSTRAR RUTINA
-            Rutina.setAttribute('animation', {
-                property: 'scale',
-                to: '.4 .4 .4',
-                easing: 'easeOutQuad',
-                dur: 800,
-            })
+                // MOSTRAR RUTINA
+                Rutina.setAttribute('animation', {
+                    property: 'scale',
+                    to: '1 1 1',
+                    easing: 'easeOutQuad',
+                    dur: 800,
+                })
             } else if (this.data.boton === '4') {
-            // MOSTRAR VIDEO
-            // Video.setAttribute('animation', {
-            // property: 'scale',
-            // to: '.8 .8 .8',
-            // easing: 'easeOutQuad',
-            // dur: 800,
-            // })
-            console.log(this.data.video)
-            const myWindow = window.open(`https://www.youtube.com/embed/${this.data.video}?rel=0&autoplay=1`,
-                '_blank', 'width=800,height=600')
-            // myWindow.document.write('<iframe width="560" height="315" src="https://www.youtube.com/embed/IYE105cTtsw" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" frameborder="0" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>')
-            //
-            // if (myWindow.requestFullscreen) {
-            // myWindow.requestFullscreen();
-            // } else if (myWindow.webkitRequestFullscreen) { // SAFARI
-            // myWindow.webkitRequestFullscreen();
-            // } else if (myWindow.msRequestFullscreen) { // IE11
-            // myWindow.msRequestFullscreen();
-            // }
-            myWindow.document.close()
+                // MOSTRAR VIDEO
+                // Video.setAttribute('animation', {
+                // property: 'scale',
+                // to: '.8 .8 .8',
+                // easing: 'easeOutQuad',
+                // dur: 800,
+                // })
+                console.log(this.data.video)
+                const myWindow = window.open(`https://www.youtube.com/embed/${this.data.video}?rel=0&autoplay=1`,
+                    '_blank', 'width=800,height=600')
+                // myWindow.document.write('<iframe width="560" height="315" src="https://www.youtube.com/embed/IYE105cTtsw" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" frameborder="0" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>')
+                //
+                // if (myWindow.requestFullscreen) {
+                // myWindow.requestFullscreen();
+                // } else if (myWindow.webkitRequestFullscreen) { // SAFARI
+                // myWindow.webkitRequestFullscreen();
+                // } else if (myWindow.msRequestFullscreen) { // IE11
+                // myWindow.msRequestFullscreen();
+                // }
+                myWindow.document.close()
             }
         }, 500)
     
-        // const newElement = document.createElement('a-entity');
-    
-        // newElement.setAttribute('position', '0 0 0')
-        // newElement.setAttribute('visible', 'false')
-        // newElement.setAttribute('scale', '0.0001 0.0001 0.0001')
-        })
-    },
+    })},
     
 });
-      
-// export {tapModelComponent}
