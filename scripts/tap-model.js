89cbb0ed565adcca1be9f87cb368b4f55f842b86
Enrique <ehenriquez@devcoast.co>
2023-01-10 12:16:49 -0500

    fix(click): fixed clicked twice.

--- scripts/tap-model.js
+++ scripts/tap-model.js
@@ -16,96 +16,100 @@ AFRAME.registerComponent('tap-model', {
         
 
         this.el.addEventListener('click', (event) => {
-            event.preventDefault();
             event.stopImmediatePropagation();
-            
-            // OCULTAR OTROS
-            if (this.data.boton) {
-                VideoAsset.pause();
-                VideoAsset.currentTime = 0;
+            event.stopPropagation();
+            event.preventDefault();
+            // console.log(event)
 
-                Combinacion.setAttribute('animation', {
-                    property: 'scale',
-                    to: '0 0 0',
-                    easing: 'easeOutQuad',
-                    dur: 500,
-                })
-                Capsulas.setAttribute('animation', {
-                    property: 'scale',
-                    to: '0 0 0',
-                    easing: 'easeOutQuad',
-                    dur: 500,
-                })
-                Rutina.setAttribute('animation', {
-                    property: 'scale',
-                    to: '0 0 0',
-                    easing: 'easeOutQuad',
-                    dur: 500,
-                })
-                Video.setAttribute('animation', {
-                    property: 'scale',
-                    to: '0 0 0',
-                    easing: 'easeOutQuad',
-                    dur: 500,
-                })
-            }
-            
-            // MOSTRAR DATOS
-            setTimeout(() => {
-                if (this.data.boton === '1') {
-                    // MOSTRAR COMBINACION
+            if (event.target.tagName == "A-ENTITY" && event.type == "click" && (event.target.id == ("boton-" + this.data.boton)) ){            
+                // OCULTAR OTROS
+                if (this.data.boton) {
+                    VideoAsset.pause();
+                    VideoAsset.currentTime = 0;
+
                     Combinacion.setAttribute('animation', {
                         property: 'scale',
-                        to: '1 1 1',
+                        to: '0 0 0',
                         easing: 'easeOutQuad',
-                        dur: 800,
+                        dur: 500,
                     })
-                
-                } else if (this.data.boton === '2') {
-                    // MOSTRAR CAPSULAS
                     Capsulas.setAttribute('animation', {
                         property: 'scale',
-                        to: '1 1 1',
+                        to: '0 0 0',
                         easing: 'easeOutQuad',
-                        dur: 800,
+                        dur: 500,
                     })
-                } else if (this.data.boton === '3') {
-                    // MOSTRAR RUTINA
                     Rutina.setAttribute('animation', {
                         property: 'scale',
-                        to: '1 1 1',
+                        to: '0 0 0',
+                        easing: 'easeOutQuad',
+                        dur: 500,
+                    })
+                    Video.setAttribute('animation', {
+                        property: 'scale',
+                        to: '0 0 0',
+                        easing: 'easeOutQuad',
+                        dur: 500,
+                    })
+                }
+                
+                // MOSTRAR DATOS
+                setTimeout(() => {
+                    if (this.data.boton === '1') {
+                        // MOSTRAR COMBINACION
+                        Combinacion.setAttribute('animation', {
+                            property: 'scale',
+                            to: '1 1 1',
+                            easing: 'easeOutQuad',
+                            dur: 800,
+                        })
+                    
+                    } else if (this.data.boton === '2') {
+                        // MOSTRAR CAPSULAS
+                        Capsulas.setAttribute('animation', {
+                            property: 'scale',
+                            to: '1 1 1',
+                            easing: 'easeOutQuad',
+                            dur: 800,
+                        })
+                    } else if (this.data.boton === '3') {
+                        // MOSTRAR RUTINA
+                        Rutina.setAttribute('animation', {
+                            property: 'scale',
+                            to: '1 1 1',
+                            easing: 'easeOutQuad',
+                            dur: 800,
+                        })
+                    } else if (this.data.boton === '4') {
+                        // MOSTRAR VIDEO
+                        Video.setAttribute('animation', {
+                        property: 'scale',
+                        to: '2 2 2',
                         easing: 'easeOutQuad',
                         dur: 800,
-                    })
-                } else if (this.data.boton === '4') {
-                    // MOSTRAR VIDEO
-                    Video.setAttribute('animation', {
-                    property: 'scale',
-                    to: '2 2 2',
-                    easing: 'easeOutQuad',
-                    dur: 800,
-                    })
+                        })
 
-                    VideoAsset.play()
+                        VideoAsset.play()
 
 
-                    // console.log(this.data.video)
-                    // const myWindow = window.open(`https://www.youtube.com/embed/${this.data.video}?rel=0&autoplay=1`,
-                    //     '_blank', 'width=800,height=600')
-                    // myWindow.document.write('<iframe width="560" height="315" src="https://www.youtube.com/embed/IYE105cTtsw" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" frameborder="0" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>')
-                    //
+                        // console.log(this.data.video)
+                        // const myWindow = window.open(`https://www.youtube.com/embed/${this.data.video}?rel=0&autoplay=1`,
+                        //     '_blank', 'width=800,height=600')
+                        // myWindow.document.write('<iframe width="560" height="315" src="https://www.youtube.com/embed/IYE105cTtsw" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" frameborder="0" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>')
+                        //
 
-                    // if (myWindow.requestFullscreen) {
-                    // myWindow.requestFullscreen();
-                    // } else if (myWindow.webkitRequestFullscreen) { // SAFARI
-                    // myWindow.webkitRequestFullscreen();
-                    // } else if (myWindow.msRequestFullscreen) { // IE11
-                    // myWindow.msRequestFullscreen();
-                    // }
+                        // if (myWindow.requestFullscreen) {
+                        // myWindow.requestFullscreen();
+                        // } else if (myWindow.webkitRequestFullscreen) { // SAFARI
+                        // myWindow.webkitRequestFullscreen();
+                        // } else if (myWindow.msRequestFullscreen) { // IE11
+                        // myWindow.msRequestFullscreen();
+                        // }
 
-                    // myWindow.document.close()
-                }
-            }, 500)
+                        // myWindow.document.close()
+                    }
+                }, 500)
+            }
     
         }, true)
     },
